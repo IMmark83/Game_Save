9183287d0085fc27d283bd2ddbbd19bd86a4d0aa
MalevichIM <malevichim@mail.ru>
2023-10-20 19:27:48 +0300

    Add class GameProgress

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameProgress gamer1 = new GameProgress(23,75,9,152.2);
+        GameProgress gamer2 = new GameProgress(28,123,15,253.8);
+        GameProgress gamer3 = new GameProgress(39,214,25,432.4);
+
+
+
+
+
+    }
+}
